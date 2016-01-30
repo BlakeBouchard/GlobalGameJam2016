2351700b4c7dce6534ee186e72e8955d248ba908
Blake Bouchard <thatsblake@gmail.com>
2016-01-29 21:41:53 -0800

    Preliminary stuff

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,29 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
+    int ConversationEnergy = 0;
+    public int ConversationGoal = 100;
+    int StressLevel = 0;
+    public int StressLimit = 10;
+    
     // Use this for initialization
     void Start()
     {
-    
+        
     }
 	
     // Update is called once per frame
     void Update()
     {
-    
+        if (StressLevel >= StressLimit)
+        {
+            Debug.Log("OH NO YOU GOT TOO STRESSED OUT");
+        }
+        
+        if (ConversationEnergy == ConversationGoal)
+        {
+            Debug.Log("You did it! You put an effort in and everybody saw it so now you can go home and watch Game of Thrones!");
+        }
     }
 }
