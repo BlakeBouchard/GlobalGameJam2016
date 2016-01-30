88ed1c14d17a614feb9f358afc156f7d51a6f819
Brett Cundal <brett.cundal@iugome.com>
2016-01-29 22:09:37 -0800

    Basic iso scene and player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{	
+	public float speed;
+	
+	void Start()
+	{
+	}
+	
+	void FixedUpdate ()
+	{
+		float horizontalInput = Input.GetAxis("Horizontal");
+		float verticalInput = Input.GetAxis("Vertical");
+
+		int moveHorizontal = ((horizontalInput > 0) ? 1 :
+			((horizontalInput < 0) ? -1 : 0));
+		int moveVertical = ((verticalInput > 0) ? 1 :
+			((verticalInput < 0) ? -1 : 0));
+
+		Vector3 movement = new Vector3(moveHorizontal * speed, 0.0f, moveVertical * speed);
+
+		GetComponent<Rigidbody>().velocity = movement;
+	}
+}
