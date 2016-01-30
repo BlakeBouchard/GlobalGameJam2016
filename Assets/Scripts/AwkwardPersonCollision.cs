b0d6fe86a5dc4a62c783d04f7a1d5d4a8d23a33b
Blake Bouchard <thatsblake@gmail.com>
2016-01-30 11:38:19 -0800

    Setting up a Player prefab with Collision script and a Collision test environment

--- Assets/Scripts/AwkwardPersonCollision.cs
+++ Assets/Scripts/AwkwardPersonCollision.cs
@@ -6,6 +6,8 @@ public class AwkwardPersonCollision : MonoBehaviour
     
     public int stressIncrease = 2;
     
+    public float fadedOpacity = 0.3f;
+    
     // Use this for initialization
     void Start()
     {
@@ -17,16 +19,19 @@ public class AwkwardPersonCollision : MonoBehaviour
         if (!hasConversedAwkwardly && collision.collider.tag == "Player")
         {
             GameManager gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
-            
             gameManager.IncreaseStress(stressIncrease);
-            
             hasConversedAwkwardly = true;
+            
+            // Fade the Awkward Person out for now
+            SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+            Color color = spriteRenderer.color;
+            color.a = fadedOpacity;
         }
     }
 	
     // Update is called once per frame
     void Update()
     {
-    
+        
     }
 }
