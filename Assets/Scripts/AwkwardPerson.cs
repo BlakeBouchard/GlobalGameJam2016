41711ae1bd22e57e46bab111ba8db4f614bcbe68
Mathias Flagel <mathias.flagel@gmail.com>
2016-01-30 13:16:43 -0800

    lost some commit history but kept the file. Awkward Person can now patrol a path with an arbitrary number of nodes. Can also use a camera to grab the frustum (array of planes in this case) to do an AABB test

--- Assets/Scripts/AwkwardPerson.cs
+++ Assets/Scripts/AwkwardPerson.cs
@@ -1,90 +1,217 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class AwkwardPerson : MonoBehaviour
 {
     public enum Behaviour
     {
-          Idle
-        , Wandering
-        , MoveToObject
-        , FleeObject
+        Idle
         , Patrolling
         , PatrolWait
+        , MoveToObject
+        , FleeObject
     }
 
     // Public
-    public GameObject game_object;
     public Behaviour current_behaviour = Behaviour.Patrolling;
-
-    public GameObject patrol_start;
-    public GameObject patrol_end;
-
     public float movement_speed = 5.0f;
+    public Quaternion target_rotation;
+    public Quaternion prev_target_rotation;
+
+    public Camera view_cam;
+    public BoxCollider player_test;
+
+    public float[] patrol_wait_time_bounds = {0.5f, 2.0f};
+
+    public List<Transform> current_path;
+    public bool path_loops = true;
+    int current_path_node_index = 0;
     //---
 
+    public Quaternion idle_original_rotation;
     Vector3 movement_target;
+    GameObject target_object = null;
+
+    float wait_timer = 0.0f;
+
+    bool HasPath { get { return current_path.Count > 0; } }
+
+    int NextPathNodeIndex
+    {
+        get{ return (current_path_node_index + 1) % current_path.Count; }
+    }
+
+    int NearestPathNodeIndex()
+    {
+        int nearest = 0;
+        float nearest_dist_sq = float.PositiveInfinity;
+
+        int i = 0;
+        foreach(var node in current_path)
+        {
+            var dist_sq = (node.position - transform.position).sqrMagnitude;
+
+            if(dist_sq < nearest_dist_sq)
+            {
+                nearest = i;
+                nearest_dist_sq = dist_sq;
+            }
+
+            i++;
+        }
+
+        return nearest;
+    }
 
     // Use this for initialization
-    void Start () {
-        movement_target = patrol_start.transform.position;
+    void Start ()
+    {
+        if(HasPath)
+        {
+            movement_target = current_path[0].position;
+        }
+
+        idle_original_rotation = transform.rotation;
+        target_rotation        = transform.rotation;
+        prev_target_rotation   = transform.rotation;
     }
 
     // Update is called once per frame
     void Update () {
         UpdateBehaviourMachine();
+
+        //testing frustum collision
+        if(CanSeePlayer(player_test.bounds))
+        {
+            GetComponent<MeshRenderer>().material.color = Color.red;
+
+            //todo: call something in GameManager?
+
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().material.color = Color.green;
+        }
+    }
+
+    bool CanSeePlayer(Bounds player_bounds)
+    {
+        Plane[] frustum_planes = GeometryUtility.CalculateFrustumPlanes(view_cam);
+        return GeometryUtility.TestPlanesAABB(frustum_planes, player_bounds);
+    }
+
+    void SetState(Behaviour new_behaviour)
+    {
+        //todo: state transitions
+        //        switch(current_behaviour)
+        //        {
+        //        };
+
+        switch(new_behaviour)
+        {
+        case Behaviour.Idle:
+            {
+                idle_original_rotation = transform.rotation;
+                break;
+            }
+
+        case Behaviour.PatrolWait:
+            {
+                wait_timer = Random.Range(patrol_wait_time_bounds[0], patrol_wait_time_bounds[1]);
+                break;
+            }
+        }
+
+        current_behaviour = new_behaviour;
     }
 
     void UpdateBehaviourMachine ()
     {
         switch (current_behaviour)
         {
-            case Behaviour.Idle:
+        case Behaviour.Idle:
             {
-                //maybe change direction every couple seconds
+                wait_timer -= Time.deltaTime;
+
+                if(wait_timer <= 0)
+                {
+                    prev_target_rotation = target_rotation;
+                    target_rotation = idle_original_rotation;
+
+                    var tmp = target_rotation.eulerAngles;
+                    tmp.x += Random.Range(-20, 20);
+                    target_rotation.eulerAngles = tmp;
+                    wait_timer = Random.Range(1.0f, 3.0f); //TODO expose as public var (or change to constant var)
+                }
+
+                transform.rotation = Quaternion.Lerp(prev_target_rotation, target_rotation, (1 - (wait_timer / 3.0f)));
+
                 break;
             }
-            case Behaviour.Patrolling:
-            {
-                //todo: handle iterating path list instead of hard coding for two points
 
-                if(game_object.transform.position == patrol_start.transform.position)
+        case Behaviour.Patrolling:
+            {
+                if(current_path.Count < 2)
                 {
-                    movement_target = patrol_end.transform.position;
-                }
-                else if(game_object.transform.position == patrol_end.transform.position)
-                {
-                    movement_target = patrol_start.transform.position;
+                    SetState(Behaviour.Idle);
                 }
 
                 MoveToward(movement_target);
-            break;
+
+                //if arrived at the current dest node (positions overlap
+                if(transform.position == current_path[current_path_node_index].position)  //FIXME use epsilon? idk if C# needs it
+                {
+                    if(current_path_node_index == current_path.Count - 1 && !path_loops)
+                    {
+                        current_path.Reverse();
+                        current_path_node_index = 0;
+                    }
+
+                    movement_target = current_path[NextPathNodeIndex].position;
+                    current_path_node_index = NextPathNodeIndex;
+                    SetState(Behaviour.PatrolWait);
+                }
+                break;
             }
-            case Behaviour.PatrolWait:
+
+        case Behaviour.PatrolWait:
             {
-                
-            break;
+                wait_timer -= Time.deltaTime;
+
+                if(wait_timer <= 0)
+                {
+                    SetState(Behaviour.Patrolling);
+                }
+                break;
             }
-        };
+
+        case Behaviour.MoveToObject:
+            {
+                break;
+            }
+        }
     }
 
     void MoveToward(Vector3 target_pos)
     {
-        var cur_pos = gameObject.transform.position;
-        var dist = target_pos - cur_pos;
+        var dist = target_pos - transform.position;
         var dir = dist.normalized;
 
-        //todo: set facing direction
+        //set facing direction
+        transform.LookAt(target_pos, Vector3.forward); //use forward to rotate around Z axis
 
         if(dist.magnitude <= movement_speed)
         {
-            game_object.transform.position = target_pos;
+            transform.position = target_pos;
         }
         else
         {
-            game_object.transform.position  += dir * movement_speed;
+            transform.position  += dir * movement_speed;
         }
     }
+
+
 
 
 }
