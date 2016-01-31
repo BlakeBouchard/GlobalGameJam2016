514769180a7372638d8af626bdaf364cdd1138f6
Blake Bouchard <thatsblake@gmail.com>
2016-01-30 17:24:28 -0800

    BROKE THE BUILD

--- Assets/Scripts/SpeechBubble.cs
+++ Assets/Scripts/SpeechBubble.cs
@@ -3,7 +3,7 @@
 public class SpeechBubble : MonoBehaviour
 {
     public float timeBetweenRolls = 1.0f;
-    public float probability = 10.0f;
+    public float probabilityPercent = 10;
     
     // Use this for initialization
     void Start()
@@ -12,10 +12,11 @@ public class SpeechBubble : MonoBehaviour
         transform.rotation = Camera.main.transform.rotation;
     }
     
-    IEnumerator RollForDialog(float waitTime)
-    {
-        yield return new WaitForSeconds(waitTime);
-    }
+    // IEnumerator RollForDialog(float waitTime)
+    // {
+    //     yield return new WaitForSeconds(waitTime);
+    //     Random.Range(0, 100);
+    // }
 	
     // Update is called once per frame
     void Update()
