240160523b08b9b0cc2fd291b469ab7e06e2fb2e
Brett Cundal <bcundal@cundal.net>
2016-01-31 14:35:53 -0800

    Fix NPC sprite

---
+++ Assets/FixedSprite.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixedSprite : MonoBehaviour {
+
+	private Quaternion orientation;
+
+	// Use this for initialization
+	void Start () {
+		orientation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.rotation = orientation;
+	}
+}
