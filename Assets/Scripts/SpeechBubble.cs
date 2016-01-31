6378a6f5931565c76eefb50b2a2794022058a7e1
Blake Bouchard <thatsblake@gmail.com>
2016-01-30 17:16:19 -0800

    Checking in some stuff

--- Assets/Scripts/SpeechBubble.cs
+++ Assets/Scripts/SpeechBubble.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class SpeechBubble : MonoBehaviour
 {
+    public float timeBetweenRolls = 1.0f;
+    public float probability = 10.0f;
+    
     // Use this for initialization
     void Start()
     {
+        // Set this object's rotation to be the same as the Main Camera
+        transform.rotation = Camera.main.transform.rotation;
+    }
     
+    IEnumerator RollForDialog(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
     }
 	
     // Update is called once per frame
     void Update()
     {
-    
+        
     }
 }
