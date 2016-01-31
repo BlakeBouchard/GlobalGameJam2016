dfe240575e346221535d583a9839e11399182375
Brett Cundal <bcundal@cundal.net>
2016-01-30 22:09:30 -0800

    Silly walks

---
+++ Assets/Scripts/BobSprite.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BobSprite : MonoBehaviour {
+
+	public GameObject sprite;
+	public float height;
+	public float tilt;
+	//public float tiltAngle;
+	public float cycleTime;
+
+	private float progress;
+	private Vector3 offset;
+	//private Quaternion tiltOffset;
+	private int tiltDirection;
+
+	void Start () {
+		progress = 0;
+		offset = sprite.transform.localPosition;
+		//tiltOffset = sprite.transform.localRotation;
+		tiltDirection = 1;
+	}
+
+	private static float SimpleBounce(float x) {
+		return Mathf.Abs(Mathf.Sin(3.14f*x));
+	}
+
+	void Update () {
+		Vector3 movement = Vector3.zero;
+		//Quaternion tiltRotation = Quaternion.identity;
+
+		bool isMoving = GetComponent<Rigidbody> ().velocity.magnitude > 0;
+
+		if (progress > 0 || isMoving) {
+			progress += Time.deltaTime / cycleTime;
+
+			float bounce = SimpleBounce(progress);
+			movement.Set(tiltDirection * tilt * bounce, height * bounce, 0);
+
+			if(progress >= 1) {
+				progress = 0;
+				tiltDirection *= -1;
+			}
+		}
+
+		sprite.transform.localPosition = offset + movement;
+		//sprite.transform.localRotation = tiltOffset * tiltRotation;
+	}
+}
