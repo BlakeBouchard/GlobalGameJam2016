1faeb9a4b2531875711fea3219e28e89a4fbd79d
Blake Bouchard <thatsblake@gmail.com>
2016-01-30 18:53:57 -0800

    Speech bubbles should work now

--- Assets/Scripts/SpeechBubble.cs
+++ Assets/Scripts/SpeechBubble.cs
@@ -1,7 +1,10 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class SpeechBubble : MonoBehaviour
 {
+    RandomDialogue randomDialogue;
+    public float lifeTime = 3.0f;
     public float timeBetweenRolls = 1.0f;
     public float probabilityPercent = 10;
     
@@ -10,17 +13,50 @@ public class SpeechBubble : MonoBehaviour
     {
         // Set this object's rotation to be the same as the Main Camera
         transform.rotation = Camera.main.transform.rotation;
+        
+        randomDialogue = GameObject.Find("Game Manager").GetComponent<RandomDialogue>();
+        
+        if (GetComponent<TextMesh>() == null)
+        {
+            Debug.LogError("Must have a GameObject attached that has a TextMesh!");
+        }
+        StartCoroutine("RollForShowBubble", timeBetweenRolls);
     }
     
-    // IEnumerator RollForDialog(float waitTime)
-    // {
-    //     yield return new WaitForSeconds(waitTime);
-    //     Random.Range(0, 100);
-    // }
+    IEnumerator RollForShowBubble(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            if (GetComponent<TextMesh>().text == "" && Random.Range(0, 100) < probabilityPercent)
+            {
+                ShowSpeechBubble();
+            }
+        }
+    }
+    
+    void ShowSpeechBubble()
+    {
+        // Enable the speech bubble
+        Debug.Log("Speech should be shown here");
+        StartCoroutine("HideSpeechBubble", lifeTime);
+        SetText(randomDialogue.GetRandomDialogueLine());
+    }
+    
+    IEnumerator HideSpeechBubble(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        SetText("");
+    }
+    
+    void SetText(string text)
+    {
+        GetComponent<TextMesh>().text = text;
+    }
 	
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.rotation = Camera.main.transform.rotation;
     }
 }
