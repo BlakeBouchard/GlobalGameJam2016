b62a8fae460f51eb205a94d2bc23bc6cd517b988
Brett Cundal <bcundal@cundal.net>
2016-01-31 13:36:49 -0800

    Hook up player, stage, arch, beer sprites; beer animation

---
+++ Assets/BobBeer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BobBeer : MonoBehaviour {
+	
+	public float height;
+	public float cycleTime;
+	
+	private Vector3 offset;
+
+	void Start () {
+		offset = transform.localPosition;
+	}
+	
+	private static float SimpleBob(float x) {
+		return Mathf.Sin (3.14f * x);
+	}
+	
+	void Update () {
+		transform.localPosition = offset + Vector3.up * (SimpleBob (Time.time / cycleTime) * height);
+	}
+}
